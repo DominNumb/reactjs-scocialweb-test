5e9e7978695a036ee5afc7a149ffff79df6c7f38
DominNumb <dominik.numb@gmail.com>
2022-04-30 14:18:50 +0200

    Added Home component

---
+++ src/components/home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    return (
+      <>
+        <h1>home screen</h1>
+      </>
+    )
+  }
+}
+
+export default home
